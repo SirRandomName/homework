29a1a088e6b556a1a6e1cd2cc9fb68c5c876a676
patrik.elekes <patrik.elekes@gmail.com>
2020-12-27 23:47:11 +0100

    feat: import reactive form to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {AuthService} from './core/auth/auth.service';
 import {LoginAuthGuard} from './core/auth/login-auth.guard';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [AuthService, LoginAuthGuard],
   bootstrap: [AppComponent]
 })
