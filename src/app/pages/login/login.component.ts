6dca57637389f8f2425f44d84809e84566249418
patrik.elekes <patrik.elekes@gmail.com>
2020-12-28 17:35:54 +0100

    feat: reactive form on login template

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -10,6 +10,7 @@ import {CostumePasswordValidators} from './password.validator';
 })
 export class LoginComponent {
   loginForm: FormGroup;
+
   constructor(private _fb: FormBuilder, private _authService: AuthService) {
     const emailValidatorPattern = new RegExp(
       "^[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$"
