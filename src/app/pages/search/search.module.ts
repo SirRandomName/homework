687191e99d722b9e640ff836bbc714ed41c3cac5
patrik.elekes <patrik.elekes@gmail.com>
2020-12-28 19:32:10 +0100

    feat: list movies

--- src/app/pages/search/search.module.ts
+++ src/app/pages/search/search.module.ts
@@ -7,10 +7,13 @@ import {ComponentsModule} from 'src/app/shared/components/components.module';
 import {HttpClientModule} from '@angular/common/http';
 import {MovieDetailsComponent} from './movie-details/movie-details.component';
 import {PipesModule} from 'src/app/shared/pipes/pipes.module';
+import {InfiniteScrollModule} from 'ngx-infinite-scroll';
+import {CardsComponent} from './cards/cards.component';
+import {CardComponent} from './cards/card/card.component';
 
 @NgModule({
-  declarations: [SearchComponent, MovieDetailsComponent],
+  declarations: [SearchComponent, MovieDetailsComponent, CardsComponent, CardComponent],
   providers: [SearchService],
-  imports: [CommonModule, SearchRoutingModule, ComponentsModule, HttpClientModule, PipesModule]
+  imports: [CommonModule, SearchRoutingModule, ComponentsModule, HttpClientModule, PipesModule, InfiniteScrollModule]
 })
 export class SearchModule {}
