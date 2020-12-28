4a175722eff4883009183c89c7bea11598330907
patrik.elekes <patrik.elekes@gmail.com>
2020-12-28 17:47:30 +0100

    feat: set env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  movieHostUrl: 'https://api.themoviedb.org/3/',
+  movieApiKey: '1c5abaaeaa13c66b570ad3042a0d51f4',
+  moviePosterHostUrl: 'https://image.tmdb.org/t/p/w300_and_h450_bestv2',
+  imdbHostUrl: 'https://www.imdb.com/'
 };
