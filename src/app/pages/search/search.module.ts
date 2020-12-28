98639758dad06f52b7c2a561d0918571a628aa58
patrik.elekes <patrik.elekes@gmail.com>
2020-12-28 17:45:00 +0100

    feat: add service for search page

--- src/app/pages/search/search.module.ts
+++ src/app/pages/search/search.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SearchComponent} from './search.component';
 import {SearchRoutingModule} from './search-routing.module';
+import {SearchService} from './search.service';
 
 @NgModule({
   declarations: [SearchComponent],
+  providers: [SearchService],
   imports: [CommonModule, SearchRoutingModule]
 })
 export class SearchModule {}
