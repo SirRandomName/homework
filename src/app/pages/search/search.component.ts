e91c37ccc82ddf66f00411c1c1c62c30031bd59c
patrik.elekes <patrik.elekes@gmail.com>
2020-12-28 18:27:19 +0100

    feat: add search functionality for search page

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Subject} from 'rxjs';
+import {ROUTING_TREE} from 'src/app/app-routing.model';
+import {IMovie, IMovieDetails, ISearchMovieResponse} from './search.model';
+import {SearchService} from './search.service';
 
 @Component({
   selector: 'app-search',
@@ -6,7 +11,40 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  constructor() {}
+  inputValue: string = '';
+  lastSearch!: string;
+  actualDetails: IMovieDetails | null = null;
+  movieSearchResults$: Subject<IMovie[]> = new Subject<IMovie[]>();
+  actualPage: number = 1;
+  totalPages: number | undefined;
+  results: IMovie[] = [];
+  totalResults: number = 0;
+  constructor(private _searchService: SearchService) {}
 
   ngOnInit(): void {}
+
+  resetSearchState() {
+    this.actualPage = 1;
+    this.totalPages = undefined;
+    this.results = [];
+    this.totalResults = 0;
+    this.movieSearchResults$.next(this.results);
+  }
+
+  search(text: string, page: number = 1): void {
+    // need to reset the previous results
+    if (this.lastSearch !== text) {
+      this.resetSearchState();
+    }
+    // do not send request if we reached the last page
+    if (this.totalPages === undefined || page <= this.totalPages) {
+      this._searchService.search(text, page).subscribe((data: ISearchMovieResponse) => {
+        this.totalPages = data.total_pages;
+        this.totalResults = data.total_results;
+        this.results.push(...data.results);
+        this.movieSearchResults$.next(this.results);
+      });
+      this.lastSearch = text;
+    }
+  }
 }
