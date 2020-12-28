dd94dfd90c06246e8175c5e3b5cd4b4577049e59
patrik.elekes <patrik.elekes@gmail.com>
2020-12-28 18:26:09 +0100

    fix: http client module import

--- src/app/pages/search/search.module.ts
+++ src/app/pages/search/search.module.ts
@@ -3,10 +3,12 @@ import {CommonModule} from '@angular/common';
 import {SearchComponent} from './search.component';
 import {SearchRoutingModule} from './search-routing.module';
 import {SearchService} from './search.service';
+import {ComponentsModule} from 'src/app/shared/components/components.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [SearchComponent],
   providers: [SearchService],
-  imports: [CommonModule, SearchRoutingModule]
+  imports: [CommonModule, SearchRoutingModule, ComponentsModule, HttpClientModule]
 })
 export class SearchModule {}
