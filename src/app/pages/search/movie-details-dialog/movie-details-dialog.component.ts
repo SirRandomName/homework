44bef9665547d20f076ecbb99e4d269f82557658
patrik.elekes <patrik.elekes@gmail.com>
2021-01-04 01:28:12 +0100

    feat: add movie details dialog

--- src/app/pages/search/movie-details-dialog/movie-details-dialog.component.ts
+++ src/app/pages/search/movie-details-dialog/movie-details-dialog.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {IMovieDetailsDialogData} from './movie-details-dialog.model';
 
 @Component({
   selector: 'app-movie-details-dialog',
@@ -6,7 +7,17 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./movie-details-dialog.component.scss']
 })
 export class MovieDetailsDialogComponent implements OnInit {
+  @Output() close: EventEmitter<void> = new EventEmitter<void>();
+  @Input() data!: IMovieDetailsDialogData;
   constructor() {}
 
   ngOnInit(): void {}
+
+  init(data: IMovieDetailsDialogData) {
+    this.data = data;
+  }
+
+  onClose(): void {
+    this.close.emit();
+  }
 }
